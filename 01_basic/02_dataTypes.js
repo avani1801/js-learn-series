4298be7d452a41d407b3de7f7e597cb2355c2b75
avani1801 <avanimist@gmail.com>
2025-02-07 18:39:31 +0530

    Add files via upload

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"Use Strict"; // treat all JS code as new version
+
+// alert( 3 + 3) // we are using nodejs, not browser.
+
+let name = "alexa";
+let age = 18;
+let isLoggedIn =false;
+
+// numer =>2 to power 53
+// bigint - 
+// string - " "  should be in double quote
+// boolean => true / false
+// null ==> standalone value - empty
+// undefined => not yet given any value to it.
+// symbol => unique
+
+
+// object 
+
+console.log(typeof age);
+
+console.log(typeof undefined); // output would be undefined
+
+console.log(typeof null); // object
